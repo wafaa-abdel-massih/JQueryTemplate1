36248552668e3a29e6608605fe185784e5b42825
wafaa-abdel-massih <wafaa2016170498@cis.asu.edu.eg>
2021-06-06 04:43:38 +0200

    added team and testimonials sections

--- js/main.js
+++ js/main.js
@@ -23,5 +23,41 @@ $(document).ready(function () {
             scrollTop: $('#' + $(this).data('value')).offset().top
         }, 1000);
     });
-    
+
+    // team image hover animation
+    $(".img-box > div").hover(function () {
+        $(this).children(".overlay").slideDown();  
+    },
+    function () {
+        $(this).children(".overlay").slideUp();
+    });
+
+    // auto slider
+    (function autoSlider() {
+
+        $(".slider .active").each(function () {
+
+            if (!$(this).is(':last-child')) {
+
+                $(this).delay(3000).fadeOut(2000, function () {
+
+                    $(this).removeClass("active").next().addClass("active").fadeIn(2000);
+
+                    autoSlider();
+
+                });
+            }
+            else {
+
+                $(this).delay(3000).fadeOut(2000, function () {
+
+                    $(this).removeClass("active");
+
+                    $('.slider div').eq(0).addClass("active").fadeIn(2000);
+
+                    autoSlider();
+                });
+            }
+        });
+    })();
 });
